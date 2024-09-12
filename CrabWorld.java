8990859a100ef6ca38060916e6b94860ed01ca7e
RealLeo0828 <jli26@sandomenico.org>
2024-09-12 11:48:37 -0700

    Update CrabWorld.java

--- CrabWorld.java
+++ CrabWorld.java
@@ -6,7 +6,6 @@ public class CrabWorld extends World
      * Create the crab world (the beach). Our world has a size 
      * of 560x560 cells, where every cell is just 1 pixel.
      */
-    
     public CrabWorld() 
     {
         super(560, 560, 1);
@@ -15,9 +14,8 @@ public class CrabWorld extends World
     
     private void prepareScene()
     {
-        System.out.println("Hello ");
-        System.out.println("World");
-        
+        System.out.println("\"Hello World\"");
+
         // instantiate new objects
         
         // add them to the world
